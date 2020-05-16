6f3f132c3fd825a4fe63cc9d772bb5f6be4ef4a3
Roie Black <roie.black@gmail.com>
2020-05-16 16:46:47 -0500

    still testing tox on travis

--- tests/sphinx/conf.py
+++
@@ -1,4 +0,0 @@
-extensions = []
-exclude_patterns = ['_build', 'tests', '_venv', '.DS_Store']
-master_doc = 'index'
-nitpicky = True
--- tests/sphinx/package/conf.py
+++ tests/sphinx/package/conf.py
@@ -1,4 +1,5 @@
 extensions = []
 exclude_patterns = ['_build', 'tests', '_venv', '.DS_Store']
+extensions = ['sphinxcontrib.lpblocks']
 master_doc = 'index'
 nitpicky = True
